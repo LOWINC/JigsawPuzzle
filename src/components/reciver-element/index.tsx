7733b91589efaec71a80e45a87e2102027212585
sunweiqi <921939025@qq.com>
2020-10-29 18:43:31 +0800

    外部组件

--- src/components/reciver-element/index.tsx
+++ src/components/reciver-element/index.tsx
@@ -1,4 +1,4 @@
-import {Banner, Block, Line, Title} from "dd-ui";
+import {Banner, Block, LayoutBlock, Line, Title} from "dd-ui";
 import React from "react";
 import {useDrop} from "react-dnd";
 import {JigsawComponents, JigsawElements} from "../../constant";
@@ -46,54 +46,110 @@ const ReciverElement: React.FC<Props> = (props) => {
   return (
     <div ref={drop} className={style["component"]}>
       <div className={style["box"]}>
-        {props.value.map((item, index) =>
-          !!item ? (
-            <Sort
-              key={`${item.type}-${index}`}
-              index={index}
-              type={item.type}
-              move={handleMove}
-              acceptType={JigsawElements}
-            >
-              <div
-                className={style["item"]}
-                onClick={() =>
-                  props.onElementSelect({
-                    elementIndex: index,
-                    elementType: item.type,
-                  })
-                }
-              >
-                {props.componentType === JigsawComponents.Title && (
-                  <Title
-                    name={`组件：${props.componentType} / 元素：${item.type}`}
-                    desc={`组件：${props.componentType} / 元素：${item.type}`}
-                  />
-                )}
-                {props.componentType === JigsawComponents.Line && (
-                  <Line
-                    name={`组件：${props.componentType} / 元素：${item.type}`}
-                    desc={`组件：${props.componentType} / 元素：${item.type}`}
-                    img='https://interest-image-dev.billbear.cn/dev/GOODS/tn-iEEAi-2tPTbMjX9CmotygqJ55YAx_08-x4WrFFPs=@big'
-                  />
-                )}
-                {props.componentType === JigsawComponents.Block && (
-                  <Block
-                    name={`组件：${props.componentType} / 元素：${item.type}`}
-                    desc={`组件：${props.componentType} / 元素：${item.type}`}
-                    img='https://interest-image-dev.billbear.cn/dev/GOODS/tn-iEEAi-2tPTbMjX9CmotygqJ55YAx_08-x4WrFFPs=@big'
-                  />
-                )}
-                {props.componentType === JigsawComponents.Swiper && (
-                  <Banner
-                    name={`组件：${props.componentType} / 元素：${item.type}`}
-                    desc={`组件：${props.componentType} / 元素：${item.type}`}
-                    img='https://interest-image-dev.billbear.cn/dev/GOODS/tn-iEEAi-2tPTbMjX9CmotygqJ55YAx_08-x4WrFFPs=@big'
-                  />
-                )}
-              </div>
-            </Sort>
-          ) : null
+        {props.componentType === JigsawComponents.Block ? (
+          <LayoutBlock>
+            {props.value.map((item, index) =>
+              !!item ? (
+                <Sort
+                  key={`${item.type}-${index}`}
+                  index={index}
+                  type={item.type}
+                  move={handleMove}
+                  acceptType={JigsawElements}
+                >
+                  <div
+                    className={style["item"]}
+                    onClick={() =>
+                      props.onElementSelect({
+                        elementIndex: index,
+                        elementType: item.type,
+                      })
+                    }
+                  >
+                    {props.componentType === JigsawComponents.Title && (
+                      <Title
+                        name={`组件：${props.componentType} / 元素：${item.type}`}
+                        desc={`组件：${props.componentType} / 元素：${item.type}`}
+                      />
+                    )}
+                    {props.componentType === JigsawComponents.Line && (
+                      <Line
+                        name={`组件：${props.componentType} / 元素：${item.type}`}
+                        desc={`组件：${props.componentType} / 元素：${item.type}`}
+                        img='https://interest-image-dev.billbear.cn/dev/GOODS/tn-iEEAi-2tPTbMjX9CmotygqJ55YAx_08-x4WrFFPs=@big'
+                      />
+                    )}
+                    {props.componentType === JigsawComponents.Block && (
+                      <Block
+                        name={`组件：${props.componentType} / 元素：${item.type}`}
+                        desc={`组件：${props.componentType} / 元素：${item.type}`}
+                        img='https://interest-image-dev.billbear.cn/dev/GOODS/tn-iEEAi-2tPTbMjX9CmotygqJ55YAx_08-x4WrFFPs=@big'
+                      />
+                    )}
+                    {props.componentType === JigsawComponents.Swiper && (
+                      <Banner
+                        name={`组件：${props.componentType} / 元素：${item.type}`}
+                        desc={`组件：${props.componentType} / 元素：${item.type}`}
+                        img='https://interest-image-dev.billbear.cn/dev/GOODS/tn-iEEAi-2tPTbMjX9CmotygqJ55YAx_08-x4WrFFPs=@big'
+                      />
+                    )}
+                  </div>
+                </Sort>
+              ) : null
+            )}
+          </LayoutBlock>
+        ) : (
+          <div>
+            {props.value.map((item, index) =>
+              !!item ? (
+                <Sort
+                  key={`${item.type}-${index}`}
+                  index={index}
+                  type={item.type}
+                  move={handleMove}
+                  acceptType={JigsawElements}
+                >
+                  <div
+                    className={style["item"]}
+                    onClick={() =>
+                      props.onElementSelect({
+                        elementIndex: index,
+                        elementType: item.type,
+                      })
+                    }
+                  >
+                    {props.componentType === JigsawComponents.Title && (
+                      <Title
+                        name={`组件：${props.componentType} / 元素：${item.type}`}
+                        desc={`组件：${props.componentType} / 元素：${item.type}`}
+                      />
+                    )}
+                    {props.componentType === JigsawComponents.Line && (
+                      <Line
+                        name={`组件：${props.componentType} / 元素：${item.type}`}
+                        desc={`组件：${props.componentType} / 元素：${item.type}`}
+                        img='https://interest-image-dev.billbear.cn/dev/GOODS/tn-iEEAi-2tPTbMjX9CmotygqJ55YAx_08-x4WrFFPs=@big'
+                      />
+                    )}
+                    {props.componentType === JigsawComponents.Block && (
+                      <Block
+                        name={`组件：${props.componentType} / 元素：${item.type}`}
+                        desc={`组件：${props.componentType} / 元素：${item.type}`}
+                        img='https://interest-image-dev.billbear.cn/dev/GOODS/tn-iEEAi-2tPTbMjX9CmotygqJ55YAx_08-x4WrFFPs=@big'
+                      />
+                    )}
+                    {props.componentType === JigsawComponents.Swiper && (
+                      <Banner
+                        name={`组件：${props.componentType} / 元素：${item.type}`}
+                        desc={`组件：${props.componentType} / 元素：${item.type}`}
+                        img='https://interest-image-dev.billbear.cn/dev/GOODS/tn-iEEAi-2tPTbMjX9CmotygqJ55YAx_08-x4WrFFPs=@big'
+                      />
+                    )}
+                  </div>
+                </Sort>
+              ) : null
+            )}
+          </div>
         )}
       </div>
     </div>
